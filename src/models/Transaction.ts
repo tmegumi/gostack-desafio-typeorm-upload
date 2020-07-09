1f0cac48f7ce2558cc8504aacee9a618e2d40f3a
Thais Megumi <tha.megumi@gmail.com>
2020-07-08 22:22:40 -0300

    feat(database): create transactions and categories tables
    Create transactions and categories tables and related then to models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,38 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column('time with time zone')
   created_at: Date;
 
+  @Column('time with time zone')
   updated_at: Date;
 }
 
