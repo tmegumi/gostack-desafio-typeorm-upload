6dd582d06719b43bed4362410c59bd593e58d867
Thais Megumi <tha.megumi@gmail.com>
2020-07-09 22:14:09 -0300

    feat(transactions): delete transaction route

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Invalid transaction id');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
